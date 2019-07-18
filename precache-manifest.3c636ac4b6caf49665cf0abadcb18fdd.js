7535164ab8067baf1863516b5d7c8bb06e347046
Olga Lyubicheva <forjobslov@gmail.com>
2019-07-18 12:11:25 +0300

    Updates

---
+++ precache-manifest.3c636ac4b6caf49665cf0abadcb18fdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e099ed2258aef2218e5d7544cffed614",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "1b12995926b01e8d77ba",
+    "url": "/react_add-todo-form/static/css/main.e66b6dfe.chunk.css"
+  },
+  {
+    "revision": "1c396f2594295a30d429",
+    "url": "/react_add-todo-form/static/js/2.09829138.chunk.js"
+  },
+  {
+    "revision": "1b12995926b01e8d77ba",
+    "url": "/react_add-todo-form/static/js/main.7d27bd90.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
